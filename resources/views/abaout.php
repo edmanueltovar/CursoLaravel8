6af5649cd23f264de625d4aee7aac84c2b02f940
edmanuelTovar <edmanuel.tovar.ocampo@gmail.com>
2021-01-28 16:42:43 -0400

    inicio de proyecto

---
+++ resources/views/abaout.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>document</title>
+    </head>
+        <body>
+            <center><h1><n>abaout funciona</n></h1></center>
+        </body>
+</html>
